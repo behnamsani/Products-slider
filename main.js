ccebe73ea5b52de03f13cdd0ed366aa1965ef0c2
Behnam Sani Ghalhar <sani.behnam14564@gmail.com>
2021-11-07 11:51:18 +0330

    Improved left and right movement and bug fixes Increase number of members and array volume

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
 let img = document.querySelectorAll(".img");
-
+let imgArr=[];
+for(let i=0;i<img.length;i++){
+    imgArr[i]=img[i];
+}
 const btnRight = document.querySelector("#arrowRight");
 const btnLeft = document.querySelector("#arrowLeft")
 const boxImg = document.querySelector("#imgBox");
 
-img[0].classList.add("pos1");
-img[1].classList.add("pos2");
-img[2].classList.add("pos3");
-img[3].classList.add("pos4");
-img[4].classList.add("pos5");
+imgArr[0].classList.add("pos1");
+imgArr[1].classList.add("pos2");
+imgArr[2].classList.add("pos3");
+imgArr[3].classList.add("pos4");
+imgArr[4].classList.add("pos5");
+
 
 
 let endPos;
 let startPos;
-let flag=true;
-let flagS=true;
-let imgArr=[];
-let addEndPos=0;
-let addStartPos=9;
+let flag=false;
+let flagS=false;
 
-for(let i=0;i<img.length;i++){
-    imgArr[i]=img[i];
-}
+let addEndPos=0;
+let addStartPos=img.length-1;
+
+
 
 btnRight.addEventListener("click",moveRight);
 btnLeft.addEventListener("click",moveLeft);
 
 
 function moveLeft(){
-    
-    imgArr.unshift(img[addStartPos]);
-        
     for(let i=0;i<imgArr.length;i++){
-        if(imgArr[i].classList[2]==="pos1"&&flagS===true){
+        if(imgArr[i].classList[2]==="pos1"&& i===0){
+            imgArr.unshift(img[addStartPos]);
+            flagS=true
+        }
+    }    
+    for(let i=0;i<imgArr.length;i++){
+        if(imgArr[i].classList[2]==="pos1"){
             startPos=i;
-            flagS=false;
         }
         imgArr[i].classList.remove("pos1");  
         imgArr[i].classList.remove("pos2");  
@@ -42,53 +46,60 @@ function moveLeft(){
         imgArr[i].classList.remove("pos4");  
         imgArr[i].classList.remove("pos5");  
     }
-    
-
         imgArr[startPos-1].classList.add("pos1");
         imgArr[startPos].classList.add("pos2");
         imgArr[startPos+1].classList.add("pos3");
         imgArr[startPos+2].classList.add("pos4");
         imgArr[startPos+3].classList.add("pos5");
-
-        console.log(imgArr);
         console.log(startPos);
     
-    
-    if(addStartPos===0){
-        addStartPos=img.length-1;
-    }else{addStartPos=addStartPos-1;}
-    console.log(addStartPos);
+    if(flagS===true){
+        if(addStartPos===0){
+            addStartPos=img.length-1;
+        }else{addStartPos=addStartPos-1;}
+        console.log(addStartPos);
+        imgArr.pop();
+        console.log(imgArr);
+    }    
 }
+
+
 function moveRight(){
-        imgArr.push(img[addEndPos]);
-        
+    for(let i=0;i<imgArr.length;i++){
+        if(imgArr[i].classList[2]==="pos5" && i===imgArr.length-1){
+            imgArr.push(img[addEndPos]);
+            flag=true
+        }
+    }    
         for(let i=0;i<imgArr.length;i++){
-            if(imgArr[i].classList[2]==="pos5"&&flag===true){
+            if(imgArr[i].classList[2]==="pos5"){
                 endPos=i;
-                flag=false;
             }
             imgArr[i].classList.remove("pos1");  
             imgArr[i].classList.remove("pos2");  
             imgArr[i].classList.remove("pos3");  
             imgArr[i].classList.remove("pos4");  
-            imgArr[i].classList.remove("pos5");  
+            imgArr[i].classList.remove("pos5");
         }
-        
-
+            
             imgArr[endPos-3].classList.add("pos1");
             imgArr[endPos-2].classList.add("pos2");
             imgArr[endPos-1].classList.add("pos3");
             imgArr[endPos].classList.add("pos4");
             imgArr[endPos+1].classList.add("pos5");
             endPos=endPos+1;
-            console.log(imgArr);
             console.log(endPos);
+            
         
+        if(flag===true){
+            console.log(addEndPos);
+            if(addEndPos===img.length-1){
+                addEndPos=0;
+            }else{addEndPos=addEndPos+1;}
+            imgArr.shift();
+            console.log(imgArr);
+        }
         
-        if(addEndPos===img.length-1){
-            addEndPos=0;
-        }else{addEndPos=addEndPos+1;}
-        console.log(addEndPos);
         
         
 }
